65e2be12c024be6198e671cef8d29877f5cd28db
physcrowley <crowlda@ecolecatholique.ca>
2022-10-12 11:54:30 -0400

    exemple de javadoc

---
+++ Documentation.java
@@ -0,0 +1,28 @@
+// en-tête du programme
+// Le format est : [Description] @author [nom et date]
+
+/** Petit programme pour montrer les commentaires de 
+ * documentation, soit les javadoc.
+ * @author physcrowley (2022-10)
+ */
+public class Documentation {
+
+    /** Fonction principale du programme */
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+     * Ceci est un commentaire de bloc (et non de 
+     * documentation... seulement 1 *)
+     * 
+     * La documentation précède uniquement :
+     *     1. les classes
+     *     2. les méthodes
+     *     3. les propriétés
+     * 
+     * Dans ce cours, on l'utilise principalement pour
+     * donner un en-tête à notre programme
+     */
+    
+}
