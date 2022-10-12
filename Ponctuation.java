e7c2c31955a78a4d671826af6d3880f65a9d1147
physcrowley <crowlda@ecolecatholique.ca>
2022-10-12 15:52:27 -0400

    ajouter un exemple de ponctuation

---
+++ Ponctuation.java
@@ -0,0 +1,21 @@
+public class Ponctuation {
+
+    public static void main(String[] args) {
+        
+        /*
+         * Les blocs de code sont enfermés entre ACCOLADES {}
+         *    - tout ce qui est dans la classe vient entre {}
+         *    - tout ce qui est dans la méthode main vient entre {}
+         *    - les instructions pour les parties d'une itération
+         *       ou d'une sélection sont enfermés entre {}
+         * 
+         * Pour mieux voir l'accolade fermante d'un bloc, on INDENTE (décale
+         * vers la droite) tout son contenu par une tabulation (4 espaces)
+         * 
+         * Les instructions se terminent toujours par un POINT-VIRGULE ;
+         * 
+         */
+        System.out.println( "Ne s'affiche pas si le ; n'est pas à la fin de cette ligne" );
+    }
+    
+}
