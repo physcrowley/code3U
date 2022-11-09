a5327615e680ce36794a1de1437fcefcb76b12f7
physcrowley <crowlda@ecolecatholique.ca>
2022-11-09 12:49:21 -0500

    Bouger les notes vers le manuel

--- Ponctuation.java
+++ Ponctuation.java
@@ -1,24 +1,7 @@
-public class Ponctuation {
-
-    public static void main(String[] args) {
-        
-        /*
-         * Les BLOCS DE CODE sont enfermés entre ACCOLADES {}
-         *    - tout ce qui est dans la classe vient entre {}
-         *    - tout ce qui est dans la méthode main vient entre {}
-         *    - les instructions pour les parties d'une itération
-         *       ou d'une sélection sont enfermés entre {}
-         * 
-         * Pour mieux voir l'accolade fermante d'un bloc, on INDENTE (décale
-         * vers la droite) tout son contenu par une tabulation (4 espaces)
-         *
-         */
-
-        /*
-         * Les INSTRUCTIONS se terminent toujours par un POINT-VIRGULE ;
-         */
-
+public class Ponctuation 
+{
+    public static void main(String[] args) 
+    {    
         System.out.println( "Ne s'affiche pas si le ; n'est pas à la fin de cette ligne" );
     }
-    
 }
