970a4950913d9a61eb5d77f1899dcdbbc39351e6
physcrowley <crowlda@ecolecatholique.ca>
2022-11-28 15:45:33 -0500

    pour comparer les String (pas juste equals)

---
+++ StringRange.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class StringRange
+{
+    public static void main(String[] args) {
+        Scanner console = new Scanner( System.console().reader() );
+        
+
+        System.out.print( "Taper un caractère de a à g > ");
+        String rep = console.next().toLowerCase();
+
+        // je veux 1 caractère dans les caractères permis
+        // String carPermis = "abcdefg";
+        // if ( rep.length() == 1 && carPermis.contains( rep ) )
+        
+        // je veux entre "a" et "g" 
+        if ( rep.compareTo("a") >= 0 && rep.compareTo("g") <= 0 ) 
+        {
+            System.out.println( "réponse valide");
+        }
+        else
+        {
+            System.out.println( "pas permis");
+        }
+
+    }
+}
